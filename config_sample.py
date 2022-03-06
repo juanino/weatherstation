00492435c5ebe5fd670e525361dd7723dcf15eec
jerry <juanino@gmail.com>
2022-03-06 14:49:37 -0500

    sample config file

---
+++ config_sample.py
@@ -0,0 +1,5 @@
+# rename this file to config.py
+# change with your info from adafruit.io
+ADAFRUIT_IO_USERNAME = "XXXX"
+ADAFRUIT_IO_KEY = "aio_XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+APIURL="https://api.openweathermap.org/data/2.5/weather?lat=41.XXXXXX&lon=-73.XXXXXX&appid=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX&units=imperial"
